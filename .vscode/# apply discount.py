f7d0d81a5a5dd49763ae0dc7b76c2c81d1fc7992
Yashvir-Rana <datasciyash@gmai.com>
2020-05-03 13:30:24 +0530

    additiona er msg

--- .vscode/# apply discount.py
+++ .vscode/# apply discount.py
@@ -2,8 +2,12 @@
 
 def apply_discount(product, discount):
     price = int(product['price'] * (1.0  - discount))
-    assert 0 <= price <= product['price']
+    assert 0 <= price <= product['price'], ['exprsn two show the error message ']
     return price
 
 shoes = {'name': 'fancy', 'price':14900}
-print(apply_discount(shoes, 0.25))
+print(apply_discount(shoes, 2))
+
+# assert syntax
+#""" assert exp1, [exp2]
+#"""
