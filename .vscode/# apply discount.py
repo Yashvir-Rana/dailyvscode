9a50043c3ec85f73e79053519b6041e3f27e7348
Yashvir-Rana <datasciyash@gmai.com>
2020-05-03 13:15:57 +0530

    assert

---
+++ .vscode/# apply discount.py
@@ -0,0 +1,9 @@
+# apply discount
+
+def apply_discount(product, discount):
+    price = int(product['price'] * (1.0  - discount))
+    assert 0 <= price <= product['price']
+    return price
+
+shoes = {'name': 'fancy', 'price':14900}
+print(apply_discount(shoes, 0.25))
