1321fe2a68832d2d83b6488c8a35359f919b3780
Yashvir-Rana <datasciyash@gmai.com>
2020-05-04 14:42:44 +0530

    comma

---
+++ .vscode/# complacement comma placement.py
@@ -0,0 +1,13 @@
+# complacement comma placement
+
+names = ['alice',
+         'bob',
+         'dilbert',
+         'gogo',]
+print(names)
+
+my_str = ('this is a super long string '
+            'spread out multiple lines '
+            'and look no backlases needed!!')
+
+print(my_str)
