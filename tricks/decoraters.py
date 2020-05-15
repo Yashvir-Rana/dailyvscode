93ede3ed0f92ee8aa9639b81bc2696ddecda49bb
Yashvir-Rana <datasciyash@gmail.com>
2020-05-15 14:44:25 +0530

    simple test

--- tricks/decoraters.py
+++ tricks/decoraters.py
@@ -1,2 +1,17 @@
-# decoraters are increse or modify the behaiour of a function.
-# 
+# decorators are increse or modify the behaiour of a function.
+# decorators alow you to extend and modify the behaiour of a callable(function, method, class)
+# without permanently modifying the callable itself.
+
+
+def deco(func):
+    def inner(a, b):
+        if a < b :
+            a, b = b, a
+        return func(a, b)
+    return inner
+
+@deco
+def div(a, b):
+    print(a/b)
+    
+div(1, 2)
